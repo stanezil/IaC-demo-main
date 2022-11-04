0d70f165b2dc6f3c46d2a2e2abbc7812785b60c4
stanezil <stanhoe@Stans-MacBook-Pro.local>
2022-11-04 11:58:44 +0800

    first

---
+++ terraform/iac-secrets.tf
@@ -0,0 +1,34 @@
+resource "kubernetes_ingress" "example_ingress" {
+  metadata {
+    name = "example-ingress"
+  }
+  spec {
+    backend {
+      service_name = "MyApp1"
+      service_port = 8080
+    }
+    rule {
+      http {
+        path {
+          backend {
+            service_name = "MyApp1"
+            service_port = 8080
+          }
+          path = "/app1/*"
+        }
+        path {
+          backend {
+            service_name = "MyApp2"
+            service_port = 8080
+          }
+          path = "/app3/*"
+          password = "geheim"
+        }
+      }
+    }
+    tls {
+      secret_name = "tls-secret"
+      aws_access_key_id="AKIAIO5FODNN7EXAMPLE"
+    }
+  }
+}
